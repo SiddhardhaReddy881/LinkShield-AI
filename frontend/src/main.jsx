a7c04043a23c05c724b38bb17bac4d9aa1b8daa0
Siddhardha Reddy <siddhardhareddy881@gmail.com>
2026-07-14 11:25:50 +0530

    Add Google Analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,25 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import ReactGA from "react-ga4";
 
 import App from "./App";
-
 import "./index.css";
-import "leaflet/dist/leaflet.css";
+
+ReactGA.initialize("G-B8EL4EK03Q");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname + window.location.search,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
     <BrowserRouter>
-
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-      />
-
+      <Toaster position="top-right" />
       <App />
-
     </BrowserRouter>
-
   </React.StrictMode>
 );
